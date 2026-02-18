5b0324c64659cb6cb3556b4189618936863cab84
Emily Wall <emily.wall@emory.edu>
2026-02-18 14:13:25 -0500

    Update vertical.js
    removed GitHub classroom content from vertical.js

--- assignment/starter-code/vertical.js
+++ assignment/starter-code/vertical.js
@@ -5,5 +5,3 @@
 // TODO Display the labels on the left hand side of the svg and the bars on the right hand side.
 // TODO The labels should be aligned so that the END of the text is aligned with the start of the bar (see reference image in the readme).
 // HINT Look into text-anchor
-
-// TODO don't forget to add, commit, and push all your files to your github repo
